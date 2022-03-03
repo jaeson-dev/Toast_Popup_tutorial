77c5e07efec880d2124e3c149ace6f97c4404608
jaeson-dev <jaeson.dev@gmail.com>
2022-03-03 23:45:41 +0900

    Initial Commit

---
+++ Toast_Popup_tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Toast_Popup_tutorial
+//
+//  Created by Jaeson.dev on 2022/03/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Toast_Popup_tutorial/Toast_Popup_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toast_Popup_tutorialApp.swift
+//  Toast_Popup_tutorial
+//
+//  Created by Jaeson.dev on 2022/03/03.
+//
+
+import SwiftUI
+
+@main
+struct Toast_Popup_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
